5fc161eeec79344106aa0a58d85864e57640647e
SSep <ssep4u@gmail.com>
2023-04-05 23:28:36 +0900

    models __str__()

--- product/models.py
+++ product/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=20)
     price = models.IntegerField(validators=[MinValueValidator(0)])  # PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.name}: {self.price}원'    #이름: 가격원
